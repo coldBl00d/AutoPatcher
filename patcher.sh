032711266346aee738009f7a90b0860e4f9c3263
Akhil Raj <akhraj@oradev.oraclecorp.com>
2018-02-21 12:17:05 +0530

    Properly switches context based on the r flag

--- patcher.sh
+++ patcher.sh
@@ -15,14 +15,13 @@ extraDelim="/"
 startPath="/scratch/aime/work/APPTOP/fusionapps/applications/crm/deploy"
 patchFolder="./orcl"
 
-rel13=false
+rel12=1 #flag for determining which release to patch, default to rel12 
 
 while getopts "r" opt; do
   case $opt in
     r)
       echo "-r was triggered, Parameter: $OPTARG" >&2
-      rel13=true
-      echo "Patching R13 Environment"
+      rel12=0
 	  ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -35,33 +34,38 @@ while getopts "r" opt; do
   esac
 done
 
-if [ -d $patchFolder ]; then 
-	touch ./orcl/bloop.jar
-	echo "If youre only seeing bloop.jar then you did not copy your jars!!"
-	for file in ./orcl/*.jar; do
-	        echo "$(basename "$file")"
-	        find  $startPath -type f -iname "$(basename "$file")" > loc.txt 
-	        #TODO this is crap but works, rewrite backup 
-	        while read location; do 
-	        	cp $location ./backup_cb 
-	        	echo "backed up server copy of " "$(basename "$file")"
-	        	break
-	        done<loc.txt
-	        while read location; do 
-	             #echo "Patching " "$(basename "$file")" " at location"
-	             folder_loc=$(echo $location | rev | cut -d"/" -f2-  | rev)
-	             folder_loc=$folder_loc$extraDelim
-	             #echo "cp " "$file " "$folder_loc " 
-	             cp $file $folder_loc
-	        done<loc.txt 
-	        echo "Patched " "$(basename "$file")"
-	        >loc.txt 
-	done
-else
-	#when orcl directory is not found, create a one and init a file in it to prevent infinite looping
-	#not sure why the loop happens 
-	echo "orcl directory not found, place all your jars there. I just created the folder for you :)"
-	echo "Please dont touch the bloop :D"
-	mkdir ./orcl
-	touch ./orcl/bloop.jar
-fi
+if [ $rel12=1 ]; then 
+	echo "***Patch tool for Release 12***"
+	if [ -d $patchFolder ]; then 
+		touch ./orcl/bloop.jar
+		echo "If youre only seeing bloop.jar then you did not copy your jars!!"
+		for file in ./orcl/*.jar; do
+		        echo "$(basename "$file")"
+		        find  $startPath -type f -iname "$(basename "$file")" > loc.txt 
+		        #TODO this is crap but works, rewrite backup 
+		        while read location; do 
+		        	cp $location ./backup_cb 
+		        	echo "backed up server copy of " "$(basename "$file")"
+		        	break
+		        done<loc.txt
+		        while read location; do 
+		             #echo "Patching " "$(basename "$file")" " at location"
+		             folder_loc=$(echo $location | rev | cut -d"/" -f2-  | rev)
+		             folder_loc=$folder_loc$extraDelim
+		             #echo "cp " "$file " "$folder_loc " 
+		             cp $file $folder_loc
+		        done<loc.txt 
+		        echo "Patched " "$(basename "$file")"
+		        >loc.txt 
+		done
+	else
+		#when orcl directory is not found, create a one and init a file in it to prevent infinite looping
+		#not sure why the loop happens 
+		echo "orcl directory not found, place all your jars there. I just created the folder for you :)"
+		echo "Please dont touch the bloop :D"
+		mkdir ./orcl
+		touch ./orcl/bloop.jar
+	fi
+else 
+	echo "***Patch tool for Release 13***"
+fi 
